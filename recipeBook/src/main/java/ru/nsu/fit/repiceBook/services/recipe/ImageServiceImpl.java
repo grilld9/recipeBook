e1f587ce9a3ca92f5279db28254be2d5ec6ed3d7
Maxim <112863178+grilld9@users.noreply.github.com>
2024-03-16 13:47:41 +0700

    Добавлено сжатие изображения

--- recipeBook/src/main/java/ru/nsu/fit/repiceBook/services/recipe/ImageServiceImpl.java
+++ recipeBook/src/main/java/ru/nsu/fit/repiceBook/services/recipe/ImageServiceImpl.java
@@ -1,7 +1,15 @@
 package ru.nsu.fit.repiceBook.services.recipe;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import javax.imageio.ImageIO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.resizers.configurations.ScalingMode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.nsu.fit.repiceBook.model.Image;
@@ -22,12 +30,19 @@ public class ImageServiceImpl implements ImageService {
     public Long saveImage(MultipartFile file) {
         Image image = new Image();
         try {
-            image.setImage(file.getBytes());
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            Thumbnails.of(ImageIO.read(file.getInputStream()))
+                .scalingMode(ScalingMode.BILINEAR)
+                .scale(0.5)
+                .outputFormat("jpg")
+                .toOutputStream(outputStream);
+            image.setImage(outputStream.toByteArray());
         } catch (IOException e) {
-            log.error("Невозможно прочитать входящий файл: " + e.getMessage());
+            log.error("Ошибка при сжатии изображения");
             throw new UncheckedIOException(e);
         }
-        image.setMediaType(file.getContentType());
+        image.setMediaType(MediaType.IMAGE_JPEG_VALUE);
+        log.info("Изображение успешно сжато и сохранино");
         return imageRepository.save(image).getId();
     }
 
